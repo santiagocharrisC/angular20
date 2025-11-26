a69a332f0cb5a5b8714e7f922a0f6ef3bcc78024
Santiago <sacharriscamargo@gmail.com>
2025-11-26 12:08:16 -0500

    basic-page

---
+++ src/app/pages/basic-page/basic-page.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-page',
+  imports: [],
+  templateUrl: './basic-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BasicPage { }
