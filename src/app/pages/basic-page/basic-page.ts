12c29433e6aece30220fa5ffdfbb69b9e33e78e1
Santiago <sacharriscamargo@gmail.com>
2025-12-01 10:01:18 -0500

    pipesss

--- src/app/pages/basic-page/basic-page.ts
+++ src/app/pages/basic-page/basic-page.ts
@@ -1,9 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+
 })
-export default class BasicPage { }
+export default class BasicPage {
+
+nameLower = signal('SANTIAGO')
+nameUpper = signal('andres')
+fullname = signal('saNtiAgO aNdReS ChaRris CaMaRgo')
+
+
+customDate = signal(new Date());
+  tickingDataEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+      console.log('tick')
+    },1000);
+
+    onCleanup(() => {
+      clearInterval(interval)
+    })
+  })
+}
