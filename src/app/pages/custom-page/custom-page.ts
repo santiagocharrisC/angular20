bcfafdf47aecd16daa9b56e5676fd7ad91537b30
Santiago <sacharriscamargo@gmail.com>
2025-12-09 10:42:53 -0500

    pipe_personalizado_169

--- src/app/pages/custom-page/custom-page.ts
+++ src/app/pages/custom-page/custom-page.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipe/toggle-case.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [
+    ToggleCasePipe
+  ],
   templateUrl: './custom-page.html',
 })
-export default class CustomPage { }
+export default class CustomPage {
+  name = signal('Santiago Charris Camargo')
+
+  upperCase = signal(true);
+}
---
+++ src/app/pipe/toggle-case.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'toggleCase'
+})
+
+export class ToggleCasePipe implements PipeTransform {
+  transform(value: string, upper:boolean = true): string {
+    return upper ? value.toUpperCase() : value.toLowerCase();
+  }
+}
